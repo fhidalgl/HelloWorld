935726de597f974c1d577ae14e8bc4b2bb86db6e
fhidalgl <fhidalgl@gmail.com>
2019-09-17 17:59:50 +0200

    Initial commit

---
+++ src/Hello.scala
@@ -0,0 +1,22 @@
+object Hello extends App {
+  def square (x: Double):Double = {
+    val y = x + 1
+
+    y + y
+  }
+
+  println("Hello, " + "Scala!")
+
+  println("Hello, Scala!".size)
+
+  println(1.to(10))
+
+  println("Hola mundo!".toUpperCase)
+
+  val radius = 10
+  val pi = 3.14159
+
+  println("Area = " + pi * radius * radius)
+
+  println( square(2))
+}
