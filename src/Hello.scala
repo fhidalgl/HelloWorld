06f6a1cbf473270b22ce37eab58387b9127a761e
fhidalgl <fhidalgl@gmail.com>
2019-09-18 08:16:44 +0200

    Prueba push - publish - pull

--- src/Hello.scala
+++ src/Hello.scala
@@ -14,7 +14,7 @@ object Hello extends App {
   println("Hola mundo!".toUpperCase)
 
   val radius = 10
-  val pi = 3.141599899
+  val pi = 3.14159989900
 
   println("Area = " + pi * radius * radius)
 
